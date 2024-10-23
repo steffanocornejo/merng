8c4e98403ac277274713efbf986437de9bc03cfd
steffanocornejo <109877672+steffanocornejo@users.noreply.github.com>
2024-10-23 10:59:01 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB:
+  'mongodb+srv://steffanocornejo1:oMghdH4EGJX5nuWl@cluster0.tspz3.mongodb.net/test?retryWrites=true',
+  SECRET_KEY: 'some very secret key'
+};
+
+// password: oMghdH4EGJX5nuWl
